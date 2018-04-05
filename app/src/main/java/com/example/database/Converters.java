ec21217dd37368b03dfd7cadcf3ce2a32e0b8361
solejar <solejar236@gmail.com>
2018-04-05 17:19:23 -0400

    Create Converters.java
    Added converter subclass

---
+++ app/src/main/java/com/example/database/Converters.java
@@ -0,0 +1,15 @@
+
+public class Converters {
+@TypeConverter
+public static ArrayList<Integer> fromString(String value) {
+    Type listType = new TypeToken<ArrayList<String>>() {}.getType();
+    return new Gson().fromJson(value, listType);
+}
+
+@TypeConverter
+public static String fromArrayList(ArrayList<Integer> list) {
+    Gson gson = new Gson();
+    String json = gson.toJson(list);
+    return json;
+}
+}
