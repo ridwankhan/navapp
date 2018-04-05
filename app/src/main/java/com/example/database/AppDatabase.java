984a5e2d7dc36858fa0d91376cdf009c9ef1d6f5
solejar <solejar236@gmail.com>
2018-04-05 17:20:20 -0400

    Made some changes to the db

--- app/src/main/java/com/example/database/AppDatabase.java
+++ app/src/main/java/com/example/database/AppDatabase.java
@@ -1,8 +1,8 @@
 package com.example.database;
-
 import android.arch.persistence.room.*;
 
 @Database(entities = {SetData.class, WorkoutData.class, ExerciseData.class }, version = 1)
+@TypeConverters({Converters.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract ExerciseDao exerciseDao();
 }
